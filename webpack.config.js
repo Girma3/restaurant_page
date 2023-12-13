da0eb25899aedfeeba649fb4dd110c647f9d1498
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-12-13 18:06:26 +0300

    css loader and font loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|webp)$/i,
+        type: 'asset/resource',
+       
+      },
+      {
+        test:/\.(woff|woff2)$/i,
+        type: 'asset/resource',
+      }
+    ],
+    
+  },
+ 
 };
