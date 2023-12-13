f5acb01e811d6692bb5c387065fe7ba073097bea
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-12-13 17:53:57 +0300

    tab logic created with function to check node element exist or not to switch page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,54 @@
-function tests(){
-    console.log("check ")
-}
-tests()
+import './style.css';
+import { firstPage } from './intro_page';
+import {menu} from './menu'
+import { createHero } from './intro_page';
+import { contact } from './contact';
+
+const content = document.querySelector('#content')
+//load intro page by default
+content.appendChild(createHero())
+firstPage()
+
+//function to check the node is present and return true to whip it and remove it that help to append another module or page(tab)
+function isInPage(node){
+  if(document.body.contains(node) == true){
+      node.textContent = "";
+      node.remove()
+      return true
+    }
+  }
+//add eventlidtener on the ancors
+const anchor = document.querySelector('ul');
+
+
+  anchor.addEventListener("click",(e)=>{
+    const intro  = document.querySelector('.intro-page');
+    const wraps = document.querySelector('.wraps')
+    const contacts = document.querySelector('.section-three')
+    if(e.target.matches("[data-index='0']")){
+     
+        if(isInPage(wraps)  || isInPage(contacts) == true){
+            firstPage();
+         }
+     }
+     else if(e.target.matches("[data-index='1']")){
+             if(isInPage(intro)  || isInPage(contacts) == true){
+                 menu();
+             }
+            
+      }
+      else if(e.target.matches("[data-index='2']")){
+            if(isInPage(intro)  || isInPage(wraps) == true){
+              contact();
+            }
+      }
+
+  });
+ 
+
+
+
+
+
+
+
