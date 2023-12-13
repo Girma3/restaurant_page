14956e496a02bb70d9bb8dd403947426c20a5428
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-12-13 17:58:57 +0300

    modeule created to export contact info page as function

---
+++ src/contact.js
@@ -0,0 +1,52 @@
+import { footer } from "./intro_page";
+const content = document.querySelector('#content')
+//function to creat location mapp on left  side and openhours on the right side
+function contacts(){
+    const section = document.createElement('div');
+    section.classList.add('section-three');
+
+    const contact = document.createElement('div');
+    contact.classList.add('contact')
+
+    const titleContact = document.createElement('h2')
+    titleContact.classList.add('contact-title');
+    titleContact.textContent = "Find Us"
+
+    const place = document.createElement('p')
+    place.textContent = "location: Addis Ababa,kotebe street"
+
+    const phone = document.createElement('p');
+    phone.textContent = "+251 9 29 04  02 27";
+
+    contact.appendChild(titleContact);
+    contact.appendChild(phone);
+    contact.appendChild(place);
+
+    const openHrs = document.createElement('div');
+    openHrs.classList.add('open-time');
+
+    const titleHrs = document.createElement('h3');
+    titleHrs.textContent = "Open Hours";
+
+    const openTime  = document.createElement('p');
+    openTime.classList.add('open-hrs');
+    openTime.textContent = `${'open 7 days a week'} 
+        ${"11:30 AM - 9 PM OR UNTIL WE SELL OUT"}`
+
+    openHrs.appendChild(titleHrs);
+    openHrs.appendChild(openTime);
+
+    const mapImg = document.createElement('div');
+    mapImg.classList.add('map-img');
+
+    contact.appendChild(openTime)
+    section.appendChild(contact);
+    section.appendChild(mapImg)
+ return section
+    
+}
+function contact(){
+    content.appendChild(contacts())
+    return content
+}
+export{contact}
